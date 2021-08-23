990f0ce75eb3e1ff06914ac5b8660e145a58f103
Jeremy Becker <jeremy.becker@twofold.swiss>
2021-08-23 13:53:45 +0200

    Added dropdown to landing page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <div class="flex justify-between p-24 absolute w-screen bottom-0">
-      <NuxtLink class="flex-initial text-5xl text-pink-600" to="/portfolio">Work</NuxtLink>
-      <NuxtLink class="flex-initial text-5xl text-indigo-600" to="/about">About</NuxtLink>
+      <div @mouseleave="dropdownOpen = false">
+        <div v-if="dropdownOpen" class="flex flex-col">
+          <NuxtLink class="flex-initial text-5xl text-indigo-600" to="/studio">Studio</NuxtLink>
+          <NuxtLink class="flex-initial text-5xl text-indigo-600" to="/portfolio">Portfolio</NuxtLink>
+        </div>
+        <span class="flex-initial text-5xl text-pink-600" to="/portfolio" @mouseenter="dropdownOpen = true">Work</span>
+      </div>
+      <NuxtLink class="flex-initial text-5xl text-indigo-600 self-end" to="/about">About</NuxtLink>
     </div>
     <TitleText />
   </div>
@@ -14,6 +20,12 @@ export default {
   layout: 'landing',
 
   components: { TitleText },
+
+  data: function () {
+        return {
+            dropdownOpen: false,
+        }
+    },
   
 }
 </script>
